460f18300a7bf68d6e953324e84c0d58ae23ed2b
alf1e <alfiewbarlow@gmail.com>
2022-10-30 19:41:50 +0000

    remove the circular import

--- backend/src/models/__init__.py
+++ backend/src/models/__init__.py
@@ -1,11 +1,6 @@
 from sqlalchemy.orm import declarative_base
 from src.plugins import dto_factory
 
-# Import Models
-from src.models.User import User
-# End Import Models
-
-
 __all__ = [
     "Base",
     "dto_factory"
